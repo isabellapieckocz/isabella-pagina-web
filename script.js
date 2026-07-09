98a21750a6f9aa7d742cb6fcc0cc6480a563a669
isabellapieckocz <isabella.pieckocz@escola.pr.gov.br>
2026-07-09 11:00:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const botao = document.getElementById("botao");
+const mensagem = document.getElementById("mensagem");
 
+botao.addEventListener("click", () => {
+    mensagem.textContent = "Obrigado por visitar a Página Web! 💙💖";
+
+    document.body.style.background =
+        "linear-gradient(135deg,#7ec8ff,#ff69b4)";
+});
